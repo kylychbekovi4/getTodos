26c52cafc85b7e7d8e63f5609687f01507219782
kylychbekovi4 <kylychbekovsanjar270@gmail.com>
2024-01-24 12:01:33 +0600

    added render

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useEffect } from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
-	const [userId, setUserId] = useState(1);
-
+	const [posts, setPosts] = useState([]);
 	const getTodos = async () => {
 		try {
 			const response = await fetch(
-				`https://jsonplaceholder.typicode.com/todos/${userId}`
+				`https://jsonplaceholder.typicode.com/posts`
 			);
 			const responseData = await response.json();
-			console.log(responseData);
+			setPosts(responseData);
 		} catch (error) {
-			console.error("Backend упал",error);
+			console.error("Backend упал", error);
 		}
 	};
 
-  useEffect (() => {
-    getTodos();
-  }, [userId])
-
-
+	useEffect(() => {
+		getTodos();
+	}, []);
 
 	return (
 		<div>
-			<input
-				type="number"
-				value={userId}
-				onChange={(e) => setUserId(e.target.value)}
-			/>
-			{/* <button onClick={getTodos}>getTodos</button> */}
+			{posts.map((item) => (
+				<div key={item.id}>
+					<h1>{item.title}</h1>
+					<p>{item.body}</p>
+				</div>
+			))}
 		</div>
 	);
 };
